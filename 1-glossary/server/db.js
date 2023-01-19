723a989deb23ce0cb1153e6bf92e30f8580b8ac7
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-01-19 11:59:24 -0800

    Get server to work. Make successful get and post requests with postman

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,27 @@
 const mongoose = require("mongoose");
 
 // 1. Use mongoose to establish a connection to MongoDB
+// main().catch(err => console.log(err));
+mongoose.connect('mongodb://localhost/glossary');
+
 // 2. Set up any schema and models needed by the app
+const glossarySchema = new mongoose.Schema({
+  word: String,
+  definition: String
+});
+
+const Glossary = mongoose.model('Glossary', glossarySchema);
+
 // 3. Export the models
+exports.save = (data) => {
+  // data is coming from app.post in server index?
+  console.log(data);
+  // return Glossary.create(data);
+  return Glossary.create({word: "word", definition: "definition"});
+}
+
+exports.getAll = () => {
+  return Glossary.find({});
+}
+
 // 4. Import the models into any modules that need them
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const { save, getAll } = require('./db.js');
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+// CONTROLLERS:
+app.post('/words', function(req, res) {
+  save(req.body)
+    .then(function(results) {
+      res.sendStatus(201);
+    })
+    .catch(function(err) {
+      res.sendStatus(400)
+    });
+});
+
+app.get('/words', function(req, res) {
+  getAll()
+    .then((data) => {
+      res.send(data)
+    });
+});
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
